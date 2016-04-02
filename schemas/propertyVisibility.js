3558ec4fbaef1a09d5f80745224aeb2f80b2fc3f
Matthew Collier <collier.matthew@gmail.com>
2016-04-01 21:12:33 -0400

    Add validation schemas.

---
+++ schemas/propertyVisibility.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012-2016 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+
+var schema = {
+  required: true,
+  title: 'Property Visibility',
+  description: 'A list of object property IRIs that are publicly visible.',
+  type: 'array',
+  uniqueItems: true,
+  items: {
+    type: 'string',
+    enum: [
+      'owner',
+      'label'
+    ]
+  },
+  errors: {
+    invalid: 'Only "owner" and "label" are permitted.',
+    missing: 'Please enter the properties that should be publicly visible.'
+  }
+};
+
+module.exports = function(extend) {
+  if(extend) {
+    return bedrock.util.extend(true, bedrock.util.clone(schema), extend);
+  }
+  return schema;
+};
