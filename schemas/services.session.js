80d4aa8449c30d1aa6c21014ac283b7a8e3670f2
Matthew Collier <collier.matthew@gmail.com>
2017-05-11 18:02:50 -0400

    Replace `postJoin` schema.

--- schemas/services.session.js
+++ schemas/services.session.js
@@ -59,6 +59,38 @@ var postPasswordReset = {
   additionalProperties: false
 };
 
+var postJoin = {
+  title: 'Join',
+  description: 'Create an Identity',
+  type: 'object',
+  properties: {
+    '@context': schemas.jsonldContext(constants.IDENTITY_CONTEXT_V1_URL),
+    id: schemas.identifier({required: false}),
+    type: {
+      required: true,
+      type: 'string',
+      enum: ['Identity']
+    },
+    sysSlug: schemas.slug(),
+    label: schemas.label(),
+    email: schemas.email(),
+    sysPassword: password(),
+    url: {
+      required: false,
+      type: 'string'
+    },
+    description: {
+      required: false,
+      type: 'string'
+    },
+    sysPublic: {
+      required: false,
+      type: visibility()
+    }
+  },
+  additionalProperties: false
+};
+
 var postLogin = {
   title: 'Login',
   description: 'Login.',
@@ -85,6 +117,9 @@ module.exports.postPassword = function() {
 };
 module.exports.postPasswordReset = function() {
   return postPasswordReset;
+};
+module.exports.postJoin = function() {
+  return postJoin;
 };
 module.exports.postLogin = function() {
   return postLogin;
